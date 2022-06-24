133a74ac83754bd3fd18a76ae48151f7c0dc342a
Iuliia <sharipova.y.s@gmail.com>
2022-06-24 19:42:20 +0200

    start work with buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,32 @@
-import React from 'react';
+import React, {MouseEvent} from 'react';
 import './App.css';
-import {Header} from "./site/Header";
-import {Body} from "./site/Body";
-import {Footer} from './site/Footer';
-import {NewComponent} from './site/NewComponent';
-import {NewComponent2} from './site/NewComponent2';
+import {Button} from './components/Button';
 
-const students = [
-    {id: 1, name: "James", age: 8},
-    {id: 2, name: "Robert", age: 18},
-    {id: 3, name: "John", age: 28},
-    {id: 4, name: "Michael", age: 38},
-    {id: 5, name: "William", age: 48},
-    {id: 6, name: "David", age: 58},
-    {id: 7, name: "Richard", age: 68},
-    {id: 8, name: "Joseph", age: 78},
-    {id: 9, name: "Thomas", age: 88},
-    {id: 10, name: "Charles", age: 98},
-    {id: 11, name: "Christopher", age: 100},
-]
-const topCars = [
-    {manufacturer:"BMW",model:'m5cs'},
-    {manufacturer:"Mercedes",model:'e63s'},
-    {manufacturer:"Audi",model:'rs6'}
-]
 function App() {
+    /*const onClickHandler = (name: string) => {
+        console.log(name)
+         }*/
+ /*const for1=()=>{
+     console.log(100200)
+ }
+ const for2=(n:number)=>{
+     console.log(n)
+ }*/
+
     return (
         <div>
-            <Header title={'New Header'}/>
-            <Body title={'New Body'}/>
-            <NewComponent students={students}/>
-            <NewComponent2 topCars={topCars}/>
-            <Footer title={'New Footer'}/>
+         {/*          <button onClick={(event: MouseEvent<HTMLButtonElement>) => onClickHandler('Vasya')}>MyYouTubeChanel-1
+            </button>
+            <button onClick={(event: MouseEvent<HTMLButtonElement>) => onClickHandler('Ilia')}>MyYouTubeChanel-2
+            </button>*/}
+          {/*  <button onClick={for1}>1</button>
+            <button onClick={()=>(for2(100200))}>2</button>*/}
+                    <Button name={'MyYouTubeChanel-1'} />
+                    <Button name={'MyYouTubeChanel-2'} />
         </div>
     );
 }
 
 export default App;
+
+
---
+++ src/components/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+type ButtonType={
+    name:string
+}
+
+export const Button=(props:ButtonType)=>{
+    return(
+        <button>{props.name}</button>
+    )
+}
